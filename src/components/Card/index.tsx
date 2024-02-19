f5e67cf888c5900fdff445f3313d07aed5fb9082
Francisco Luca Crema <francisco.crema@miracledevs.com>
2024-02-19 13:50:29 -0300

    add loading component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,40 +1,58 @@
 import { IInformation } from "@/models/IInformation";
 import styles from "./Card.module.scss";
+import Loader from "@/components/Card/Loader";
 /* import Loader from "./Loader"; */
 
 /* Llega data como prop de App.tsx, y muestra los datos del json */
 
-function Card({ data }: { data: IInformation | null }) {
-  /*  console.log("informacion:", data); */
+function Card({
+  data,
+  loading,
+}: {
+  data: IInformation | null;
+  loading: boolean;
+}) {
   return (
     <div className={styles.info_container}>
       <div className={styles.info_region}>
-        {data ? (
-          <ul className={styles.region_ul}>
-            <li className={styles.list}>
-              <div className={styles.list_location}>
-                <h2>
-                  Tiempo en{" "}
-                  <span className={styles.span}>{data.location.name} </span> -{" "}
-                  <span className={styles.span}>{data.location.country} </span>!
-                </h2>
-              </div>
-            </li>
-            <li className={styles.list}>
-              <div className={styles.temperature}>
-                <img src={data.current.condition.icon} alt="icon" />
-                <p>{data.current.temp_c}°C</p>
-              </div>
-            </li>
-            <li className={styles.list}>
-              <p>{data.current.condition.text}</p>
-            </li>
-            <li className={styles.list}>
-              <p>{data.location.localtime}</p>
-            </li>
-          </ul>
+        {loading ? (
+          <Loader />
         ) : (
-          <p>No se encuentra el valor solicitado</p>
+          <>
+            {data ? (
+              <ul className={styles.region_ul}>
+                <li className={styles.list}>
+                  <div className={styles.list_location}>
+                    <h2>
+                      Tiempo en{" "}
+                      <span className={styles.span}>{data.location.name} </span>{" "}
+                      -{" "}
+                      <span className={styles.span}>
+                        {data.location.country}{" "}
+                      </span>
+                      !
+                    </h2>
+                  </div>
+                </li>
+                <li className={styles.list}>
+                  <div className={styles.temperature}>
+                    <img src={data.current.condition.icon} alt="icon" />
+                    <p>{data.current.temp_c}°C</p>
+                  </div>
+                </li>
+                <li className={styles.list}>
+                  <p>{data.current.condition.text}</p>
+                </li>
+                <li className={styles.list}>
+                  <p>{data.location.localtime}</p>
+                </li>
+              </ul>
+            ) : (
+              <p className={styles.noData}>
+                No se encuentra el valor solicitado
+              </p>
+            )}
+          </>
         )}
       </div>
     </div>
--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -5,23 +5,25 @@ import InputContainer from "./InputContainer";
 
 function Weather() {
   const [searchResults, setSearchResults] = useState<IInformation | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   const fetchData = async (city: string) => {
+    setLoading(true);
     const URL = `${process.env.API_URL_PARAMS}?key=${process.env.WEATHER_API_KEY}&q=${city}`;
-    const response = await fetch(URL);
-    /* console.log(response); */
-    if (!response.ok) {
-      return null;
-    }
-    const data = await response.json();
-    /*  console.log(data); */
-    setSearchResults(data);
+    return await fetch(URL).then(async (res) => {
+      setLoading(false);
+      if (!res.ok) return null;
+      else {
+        const response = await res.json();
+        setSearchResults(response);
+      }
+    });
   };
 
   return (
     <>
       <InputContainer fetchData={fetchData} />
-      <Card data={searchResults} />
+      <Card data={searchResults} loading={loading} />
     </>
   );
 }
