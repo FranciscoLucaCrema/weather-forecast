62c6770f5f2b9c7ae89875e02512062837ddf669
Francisco Crema <francisco.crema@miracledevs.com>
2024-02-22 15:00:03 -0300

    responsive dising

--- src/components/Cards/CardSecondary/index.tsx
+++ src/components/Cards/CardSecondary/index.tsx
@@ -7,8 +7,14 @@ function CardSecondary({ data }: { data: IForecastDay }) {
     <section className={styles.info_subRegion}>
       <div className={styles.date}>
         <img src={data.day.condition.icon} alt="Icon" />
-        <p className={styles.condition}>Max: {data.day.maxtemp_c}°C</p>
-        <p className={styles.condition}>Min: {data.day.mintemp_c}°C</p>
+        <div className={styles.condition}>
+          <span>Max:</span>
+          <span>{data.day.maxtemp_c}°C</span>
+        </div>
+        <div className={styles.condition}>
+          <span>Min:</span>
+          <span>{data.day.mintemp_c}°C</span>
+        </div>
         <p className={styles.condition}>{data.day.condition.text}</p>
       </div>
 
