f719f64c2df6e35d165b75bcafa8336948b37ed3
Francisco Crema <francisco.crema@miracledevs.com>
2024-02-26 16:51:32 -0300

    show more than 1 day WIP

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -6,28 +6,28 @@ import InputContainer from "./InputContainer";
 import Loader from "@/components/Shared/Loader";
 import styles from "./Weather.module.scss";
 import "@/App.scss";
+import WeatherService from "@/services/WeatherService";
 
 function Weather() {
   const [searchResults, setSearchResults] = useState<IInformation | null>(null);
   const [errorMessage, setErrorMessage] = useState<Error | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const fetchData = async (city: string) => {
+  const fetchData = async (city: string, days: number = 4) => {
     setLoading(true);
     /* maximum number of days allowed to show the API*/
-    const days = 4;
-    const URL = `${process.env.API_URL_PARAMS}?key=${process.env.WEATHER_API_KEY}&q=${city}&days=${days}`;
-    return await fetch(URL).then(async (res) => {
-      setLoading(false);
-      const jsonResponse = await res.json();
-      if (!res.ok) {
-        setErrorMessage(new Error("Your search didn't throw any results"));
-        setSearchResults(null);
-      } else {
-        setSearchResults(jsonResponse);
-        setErrorMessage(null);
-      }
-    });
+
+    const weatherService = new WeatherService();
+    const response = await weatherService.getWeather(city, days);
+    setLoading(false);
+
+    if (!response) {
+      setErrorMessage(new Error("Your search didn't throw any results"));
+      setSearchResults(null);
+    } else {
+      setSearchResults(response);
+      setErrorMessage(null);
+    }
   };
 
   return (
@@ -37,11 +37,12 @@ function Weather() {
         <div className={styles.cards}>
           <CardPrimary data={searchResults} />
           <div className={styles.cardSecondary}>
-            {searchResults.forecast.forecastday.map((day, index) => {
-              //ignore the first element of the forecastday array (today)
-              if (index === 0) return;
-              return <CardSecondary key={index} data={day} />;
-            })}
+            {searchResults.forecast.forecastday.length > 1 &&
+              searchResults.forecast.forecastday.map((day, index) => {
+                //ignore the first element of the forecastday array (today)
+                if (index === 0) return;
+                return <CardSecondary key={index} data={day} index={index} />;
+              })}
           </div>
         </div>
       )}
