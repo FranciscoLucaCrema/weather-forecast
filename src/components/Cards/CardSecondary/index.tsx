6ff8622107414634d36f6fa3bd099ce7b3552d92
Francisco Crema <francisco.crema@miracledevs.com>
2024-02-27 16:09:22 -0300

    carousel WIP

--- src/components/Cards/CardSecondary/index.tsx
+++ src/components/Cards/CardSecondary/index.tsx
@@ -5,21 +5,23 @@ import { formatDay } from "@/utils/DateFormat";
 function CardSecondary({ data, index }: { data: IForecastDay; index: number }) {
   return (
     <section className={styles.info_subRegion}>
-      <div className={styles.date}>
-        <img src={data.day.condition.icon} alt="Icon" />
-        <div className={styles.condition}>
-          <span>Max:</span>
-          <span>{data.day.maxtemp_c}°C</span>
+      <div className={styles.cards}>
+        <div className={styles.date}>
+          <img src={data.day.condition.icon} alt="Icon" />
+          <div className={styles.condition}>
+            <span>Max:</span>
+            <span>{data.day.maxtemp_c}°C</span>
+          </div>
+          <div className={styles.condition}>
+            <span>Min:</span>
+            <span>{data.day.mintemp_c}°C</span>
+          </div>
+          <p className={styles.condition}>{data.day.condition.text}</p>
         </div>
-        <div className={styles.condition}>
-          <span>Min:</span>
-          <span>{data.day.mintemp_c}°C</span>
-        </div>
-        <p className={styles.condition}>{data.day.condition.text}</p>
-      </div>
 
-      <div className={styles.days}>
-        <p>{index === 1 ? "Tomorrow" : formatDay(data.date, "long")}</p>
+        <div className={styles.days}>
+          <p>{index === 1 ? "Tomorrow" : formatDay(data.date, "long")}</p>
+        </div>
       </div>
     </section>
   );
