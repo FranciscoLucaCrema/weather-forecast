b41224ff834e4c1e66021e0a2072cc42d816b191
Francisco Crema <francisco.crema@miracledevs.com>
2024-02-28 12:41:57 -0300

    creation of method to take mock data

--- src/services/WeatherService.ts
+++ src/services/WeatherService.ts
@@ -1,4 +1,4 @@
-import { IInformation } from "@/models/IInformation";
+import { IForecastDay, IInformation } from "@/models/IInformation";
 
 class WeatherService {
   private readonly url: string;
@@ -7,15 +7,27 @@ class WeatherService {
     this.url = `${process.env.API_URL_PARAMS}?key=${process.env.WEATHER_API_KEY}`;
   }
 
-  async getWeather(city: string, days: number): Promise<IInformation | null> {
+  async getWeather(city: string): Promise<IInformation | null> {
     /*  const url = `${process.env.API_URL_PARAMS}?key=${process.env.WEATHER_API_KEY}`; */
-    const response = await fetch(`${this.url}&q=${city}&days=${days}`).then(
-      async (res) => {
-        if (res.ok) return await res.json();
-        else return null;
-      }
-    );
+    const response = await fetch(`${this.url}&q=${city}`).then(async (res) => {
+      if (res.ok) return await res.json();
+      else return null;
+    });
     return response;
+  }
+
+  async getMockDays(): Promise<IForecastDay[] | null> {
+    try {
+      const response = await fetch("./src/mock.json");
+      if (response.ok) {
+        const data = await response.json();
+        return data as IForecastDay[];
+      } else {
+        throw new Error("Error al obtener los datos del mock");
+      }
+    } catch (error) {
+      return null;
+    }
   }
 }
 
